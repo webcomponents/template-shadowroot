d2c695e6a26dd077f419e53e479da6b251275e62
Peter Burns <rictic@google.com>
2020-03-09 20:39:08 -0700

    Test in more browsers

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,3 @@
-// Karma configuration
-// Generated on Mon Mar 09 2020 18:48:51 GMT-0700 (Pacific Daylight Time)
-
 module.exports = function(config) {
   config.set({
     // base path that will be used to resolve all patterns (eg. files, exclude)
@@ -9,6 +6,8 @@ module.exports = function(config) {
     files: [
       { pattern: 'test/*.js', type: 'module' }
     ],
+    exclude: [],
+
     plugins: [
       // load plugin
       require.resolve('@open-wc/karma-esm'),
@@ -16,56 +15,45 @@ module.exports = function(config) {
       // fallback: resolve any karma- plugins
       'karma-*',
     ],
-    // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['esm', 'jasmine'],
+    frameworks: ['esm', 'jasmine', 'detectBrowsers'],
     esm: {
       // if you are using 'bare module imports' you will need this option
       nodeResolve: true,
     },
-
-    // list of files / patterns to exclude
-    exclude: [
-    ],
-
-
-    // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
     },
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress'],
-
-
-    // web server port
     port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['ChromeHeadless', 'ChromeWithDeclarativeShadowDOM'],
 
     customLaunchers: {
       ChromeWithDeclarativeShadowDOM: {
         base: 'ChromeCanaryHeadless',
+        browserName: 'ChromeWithDeclarativeShadowDOM',
         flags: ['--enable-blink-features=DeclarativeShadowDOM']
+      }
+    },
+
+    detectBrowsers: {
+      enabled: true,
+      usePhantomJS: false,
+      preferHeadless: true,
+
+      // post processing of browsers list
+      // here you can edit the list of browsers used by karma
+      postDetection: function(availableBrowsers) {
+        // This is for fast headless testing, filter out the non-headless
+        // browsers.
+        availableBrowsers = availableBrowsers.filter(b => /headless/i.test(b));
+        if (availableBrowsers.includes('ChromeCanaryHeadless')) {
+          availableBrowsers.push('ChromeWithDeclarativeShadowDOM');
+        }
+        return availableBrowsers;
       }
     },
 
@@ -76,5 +64,5 @@ module.exports = function(config) {
     // Concurrency level
     // how many browser should be started simultaneous
     concurrency: Infinity
-  })
+  });
 }
